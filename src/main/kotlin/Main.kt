422f2b853cd144d8f339b7549739869943c63031
algor1 <alexeev.igor.v@gmail.com>
2025-08-01 00:18:36 +0200

    remove unused code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,12 +17,11 @@ import io.modelcontextprotocol.kotlin.sdk.server.mcp
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>): Unit = runBlocking {
-    val command = args.firstOrNull() ?: "--sse-server-ktor"
     val port = args.getOrNull(1)?.toIntOrNull() ?: 3001
     runSseMcpServerUsingKtorPlugin(port)
 }
 
-suspend fun runSseMcpServerUsingKtorPlugin(port: Int): Unit {
+suspend fun runSseMcpServerUsingKtorPlugin(port: Int) {
     println("Starting sse server on port $port")
     println("Use inspector to connect to the http://localhost:$port/sse")
 
